6ecada29952acb3701ad2c54d98b46b1ab08d50b
Cemre YARDIM <cemreyardim@gmail.com>
2023-07-23 22:47:55 +0300

    user auth & testing API

--- restaurant/models.py
+++ restaurant/models.py
@@ -16,4 +16,4 @@ class Menu(models.Model):
     inventory = models.IntegerField(default = 0)
     
     def __str__(self):
-        return self.title
+        return f'{self.title} : {str(self.price)}'
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('items', views.MenuItemView.as_view()),
+    path('items', views.MenuItemView.as_view(), name='menu-items'),
     path('items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token)
 ]
